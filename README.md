5bc6dbaf0f79d4d6a6126e06ed79c8d87b7607bf
Puzzle Inc <puzzleinc.design@gmail.com>
2022-10-20 15:24:54 +0700

    tambah fungsi

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # alarm-pompa
 
-ESP01-1MB
+ESP01-1M
 Base on ESP8266
+
+ESP01 1M yang diprogram dengan menggunakan PlatformIO sebelum menggunakan File Sisyem harus di di inisiasi pada file platform.ini
+Kode ini menggunakna FS untuk menyimpan history waktu nyala sebelumnya, sebanyak 4 waktu akan disimpan dalam bentuk json
+
+Inisiasi FS pada ESP01 menggunakan :
+- board_build.ldscript
+- board_build.filesystem
+
+IP menggunakan IP Statis dengan alamat 192.168.0.190
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LittleFS.h>
+#include <ArduinoJson.h>
 
 #include "wificonnect.h"
 
@@ -21,7 +22,7 @@ const char* deviceName;
 AsyncWebServer server(80);
 // Make variable prototype -----------------------
 void handleRequest(AsyncWebServerRequest *request);
-void getTimehistory(AsyncWebServerRequest *request);
+void getTimesetting(AsyncWebServerRequest *request);
 
 // Define millis variable #1 ------------------------
 const unsigned long dangerInterval = 1800000; // 30 menit
@@ -30,14 +31,22 @@ const unsigned long okInterval = 0;
 unsigned long previousTime = 0;
 
 void setup() {
+  // Initialize Serial port and I2C pin -----------------------
   Serial.begin(115200);
   while (!Serial); // wait for serial port to connect. Needed for native USB
 
+  // Initialize peripehalps pin -----------------------
   /*  Common cathode led RGB */
   pinMode(buzz, OUTPUT);
   pinMode(redled, OUTPUT);
   pinMode(greenled, OUTPUT);
   pinMode(blueled, OUTPUT);
+
+  // Initialize filesystem -----------------------
+  if(!LittleFS.begin()){
+    Serial.println("An Error has occurred while mounting LittleFS");
+    return;
+  }
 
   //  Connecting to wifi -----------------------
   ssid = "Puzzle24";  // Enter SSID here
@@ -45,11 +54,38 @@ void setup() {
   deviceName = "Sensor Pompa"; // DHCP Hostname (useful for finding device for static lease)
   wificonnect(ssid, password, deviceName);
 
-  Serial.println(WiFi.gatewayIP());
+  server.on("/post", HTTP_POST,[](AsyncWebServerRequest * request){},NULL,[](AsyncWebServerRequest * request, uint8_t *data, size_t len, size_t index, size_t total) {
+
+    String json = "";
+    for (size_t i = 0; i < len; i++) {
+      json += (char)data[i]; //binary strean to char array
+    }
+
+    File this_file = LittleFS.open("/config.json", "w");
+    if (!this_file) { // failed to open the file, return false
+      const char* openfail = "Failed to open file for reading";
+      request->send(200, "text/plain", openfail);
+      return;
+    }
+    int bytesWritten = this_file.print(json);
+
+    if (bytesWritten == 0) { // write failed
+      const char *writefail = "Gagal menulis data";
+      request->send(200, "text/plain", writefail);
+      return;
+    }
+
+    const char *writedone = "Berhasil menulis data, resetting device..";
+    request->send(200, "text/plain", writedone);
+    this_file.close();
+
+    delay(2000);
+    //ESP.restart(); //ESP.reset();    
+  });
 
     //  Creating handle request from client -----------------------
   server.on("/", HTTP_ANY, handleRequest);
-  server.on("/history", HTTP_ANY, getTimehistory);
+  server.on("/setting", HTTP_ANY, getTimesetting);
   server.begin();
 }
 
@@ -82,7 +118,7 @@ void handleRequest(AsyncWebServerRequest *request){
   request->send(200, "text/plain", "Waktu berjalan = " + String(thisTime) + " menit");
 }
 
-void getTimehistory(AsyncWebServerRequest *request) {
+void getTimesetting(AsyncWebServerRequest *request) {
     String result = "";
     File file = LittleFS.open("/data.json", "r");
 
